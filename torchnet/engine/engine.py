033f061a1c4a9efa8e934ddf96d336f20cc9cb32
Noah Golmant <noah.golmant@gmail.com>
2018-04-16 14:30:33 -0700

    add correct mini-batch division for batch norm stats

--- torchnet/engine/engine.py
+++ torchnet/engine/engine.py
@@ -1,8 +1,8 @@
 class Engine(object):
-    def __init__(self, create_graph=False, update_period=1):
+    def __init__(self, create_graph=False, mini_batch_size=-1):
         self.hooks = {}
         self.create_graph = create_graph
-        self.update_period = update_period
+        self.mini_batch_size = mini_batch_size
 
     def hook(self, name, state):
         if name in self.hooks:
@@ -23,25 +23,35 @@ class Engine(object):
         while state['epoch'] < state['maxepoch']:
             self.hook('on_start_epoch', state)
             for sample in state['iterator']:
-                state['sample'] = sample
-                self.hook('on_sample', state)
+                inputs = Variable(cast(sample[0], 'float'))
+                targets = Variable(cast(sample[1], 'long'))
 
-                def closure():
-                    loss, output = state['network'](state['sample'])
-                    state['output'] = output
-                    state['loss'] = loss
-                    loss.backward(create_graph=self.create_graph, retain_graph=self.create_graph)
-                    self.hook('on_forward', state)
-                    # to free memory in save_for_backward
-                    state['output'] = None
-                    state['loss'] = None
-                    return loss
-                # Allows us to do larger batch sizes.
-                # Only do this every BS/MAX_BS iterations.
-                if state['t'] % self.update_period == 0:
-                    state['optimizer'].zero_grad()
-                    state['optimizer'].step(closure)
-                    self.hook('on_update', state)
+                batch_size = len(inputs)
+
+                # Chunk into equally sized mini-batches for batch size statistics
+                mini_inputs = inputs.chunk(batch_size // self.mini_batch_size)
+                mini_targets = targets.chunk(batch_size // self.mini_batch_size)
+                mini_samples = zip(mini_inputs, mini_targets)
+
+                state['optimizer'].zero_grad()
+                for k, mini_sample in enumerate(mini_samples):
+                    state['sample'] = mini_sample
+                    self.hook('on_sample', state)
+
+                    def closure():
+                        loss, output = state['network'](state['sample'])
+                        state['output'] = output
+                        state['loss'] = loss
+                        loss.backward(create_graph=self.create_graph, retain_graph=self.create_graph)
+                        self.hook('on_forward', state)
+                        # to free memory in save_for_backward
+                        state['output'] = None
+                        state['loss'] = None
+                        return loss
+
+                state['optimizer'].step(closure, scaling_factor=len(mini_inputs), grad_clip=5.)
+                self.hook('on_update', state)
+
                 state['t'] += 1
             state['epoch'] += 1
             self.hook('on_end_epoch', state)
